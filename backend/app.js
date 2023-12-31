ee6f8eea7773a9a528e3846dce04c07d1ab2fc83
shabkhanshab <khanshab9343@gmail.com>
2023-12-31 15:20:07 +0530

    add links

--- backend/app.js
+++ backend/app.js
@@ -30,7 +30,7 @@ cloudinary.config({
 })
 
 app.use(cookieParser())
-app.use(cors({origin:"http://localhost:3000",credentials:true}))
+app.use(cors({origin:"https://webearn.vercel.app/",credentials:true}))
 
 app.use(express.json())
 
